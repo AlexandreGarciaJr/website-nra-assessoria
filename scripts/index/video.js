2307410f0fad2737c85f7b40327af3e0ace56d49
AlexandreGarciaJr <alexandregojunior@gmail.com>
2026-05-08 04:18:16 -0300

    video test

--- scripts/index/video.js
+++ scripts/index/video.js
@@ -2,12 +2,29 @@
 const heroVideo = document.getElementById('hero-video');
 const isMobile = window.innerWidth <= 768;
 
-const playlist = isMobile ? [
+// const playlist = isMobile ? [
+//   './assets/mp4/Mixkit-Natural-Landscape-para-mobile.mp4',
+//   './assets/mp4/mixkit-forest-in-the-mountains-para-mobile.mp4'
+// ] : [
+//   './assets/mp4/mixkit-natural-landscape-in-an-aerial-view-around-a-forest-41378-full-hd.mp4',
+//   './assets/mp4/mixkit-forest-in-the-mountains-aerial-view-1764-full-hd.mp4'
+// ];
+
+// let heroIndex = 0;
+// heroVideo.src = playlist[0];
+// heroVideo.load();
+// heroVideo.play().catch(() => {});
+
+// heroVideo.addEventListener('ended', () => {
+//   heroIndex = (heroIndex + 1) % playlist.length;
+//   heroVideo.src = playlist[heroIndex];
+//   heroVideo.play().catch(() => {});
+// });
+
+
+const playlist = [
   './assets/mp4/Mixkit-Natural-Landscape-para-mobile.mp4',
   './assets/mp4/mixkit-forest-in-the-mountains-para-mobile.mp4'
-] : [
-  './assets/mp4/mixkit-natural-landscape-in-an-aerial-view-around-a-forest-41378-full-hd.mp4',
-  './assets/mp4/mixkit-forest-in-the-mountains-aerial-view-1764-full-hd.mp4'
 ];
 
 let heroIndex = 0;
